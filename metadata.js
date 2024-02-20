90b7829d97dd993cd8ac38a6ff4161f638a44b71
Marc Palau <152580332+MarcQweebi@users.noreply.github.com>
2024-02-20 16:42:26 +0900

    twitter metadata

--- metadata.js
+++ metadata.js
@@ -14,12 +14,39 @@ function generateMetadata(title, description, imageUrl, url) {
     const metadata4 = document.createElement('meta');
     metadata4.setAttribute('property', 'og:url');
     metadata4.setAttribute('content', url);
+
+    const metadataTwitter = document.createElement('meta');
+    metadataTwitter.setAttribute('name', 'twitter:card');
+    metadataTwitter.setAttribute('content', 'summary_large_image');
+    metadataTwitter.setAttribute('name', 'twitter:site');
+    metadataTwitter.setAttribute('content', '@voxelvanguard');
+    
+    const metadataTwitter2 = document.createElement('meta');
+    metadataTwitter2.setAttribute('name', 'twitter:title');
+    metadataTwitter2.setAttribute('content', title);
+    metadataTwitter2.setAttribute('name', 'twitter:description');
+    metadataTwitter2.setAttribute('content', description);
+
+    const metadataTwitter3 = document.createElement('meta');
+    metadataTwitter3.setAttribute('name', 'twitter:image');
+    metadataTwitter3.setAttribute('content', imageUrl);
+
+    const metadataTwitter4 = document.createElement('meta');
+    metadataTwitter4.setAttribute('name', 'twitter:url');
+    metadataTwitter4.setAttribute('content', url);
     
     const head = document.querySelector('head');
+    
     head.appendChild(metadata);
     head.appendChild(metadata2);
     head.appendChild(metadata3);
     head.appendChild(metadata4);
+
+    head.appendChild(metadataTwitter);
+    head.appendChild(metadataTwitter2);
+    head.appendChild(metadataTwitter3);
+    head.appendChild(metadataTwitter4);
+
 }
 
 // Example usage:
